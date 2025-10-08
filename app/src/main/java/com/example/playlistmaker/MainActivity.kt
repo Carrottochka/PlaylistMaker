c491b7246385277043a65c8cd482021791bebd82
Carrottochka <carrottochka@gmail.com>
2025-10-08 20:23:59 +0300

    Sprint 8

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,16 +1,50 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
+
+        val button1 = findViewById<MaterialButton>(R.id.button1)
+        button1.setOnClickListener {
+            val intent = Intent(this, SearchActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        /* val button1ClickListener: View.OnClickListener = object: View.OnClickListener{
+             override fun onClick(v: View?) {
+
+                 Toast.makeText(this@MainActivity, "Нажали на кноку!", Toast.LENGTH_LONG).show()
+             }
+         }
+             button1.setOnClickListener(button1ClickListener)*/
+
+        val button2 = findViewById<MaterialButton>(R.id.button2)
+        button2.setOnClickListener {
+            val intent = Intent(this, MediatekaActivity::class.java)
+            startActivity(intent)
+            // Toast.makeText(this@MainActivity, "Нажали на кнопку!", Toast.LENGTH_LONG).show()
+        }
+
+        val button3 = findViewById<MaterialButton>(R.id.button3)
+        button3.setOnClickListener {
+            val intent = Intent(this, SettingActivity::class.java)
+            startActivity(intent)
+            //Toast.makeText(this@MainActivity, "Нажали на кнопку!", Toast.LENGTH_LONG).show()
+        }
 
     }
 }
---
+++ app/src/main/java/com/example/playlistmaker/SettingActivity.kt
@@ -0,0 +1,35 @@
+package com.example.playlistmaker
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
+
+class SettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_settings)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.toolbarBack)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        val toolbarBack = findViewById<Toolbar>(R.id.toolbarBack)
+        setSupportActionBar(toolbarBack)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        toolbarBack.setOnClickListener {
+            println("Кнопка back нажата!")
+            finish()
+
+        }
+    }
+}
