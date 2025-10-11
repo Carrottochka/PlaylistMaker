6833d63c364d3cb9975b19228d9329976d976bd9
Carrottochka <carrottochka@gmail.com>
2025-10-11 19:42:29 +0300

    Sprint 8 last chance

--- app/src/main/java/com/example/playlistmaker/SettingActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingActivity.kt
@@ -12,13 +12,9 @@ import com.google.android.material.button.MaterialButton
 class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_settings)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.toolbarBack)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
 
 
         val toolbarBack = findViewById<Toolbar>(R.id.toolbarBack)
