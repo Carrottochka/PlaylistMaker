ad4dc6cad87f219ad926bb6d62f5f2ce7188e7c4
Carrottochka <carrottochka@gmail.com>
2025-11-01 08:35:35 +0300

    Sprint 9 FIRST try

--- app/src/main/java/com/example/playlistmaker/SettingActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingActivity.kt
@@ -1,29 +1,82 @@
 package com.example.playlistmaker
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.button.MaterialButton
+
 
 class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_settings)
-
-
-
         val toolbarBack = findViewById<Toolbar>(R.id.toolbarBack)
-
-
         toolbarBack.setOnClickListener {
             println("Кнопка back нажата!")
             finish()
+        }
+        setupShareButton()
+        setupContackSupport()
+        setupUserAgreement()
+    }
+
+    fun setupShareButton() {
+        val shareApp = findViewById<TextView>(R.id.shareApp)
+        shareApp.setOnClickListener {
+            val intentShare = Intent(Intent.ACTION_SEND)
+            intentShare.type = "text/plain"
+            intentShare.putExtra(
+                Intent.EXTRA_TEXT,
+                "https://practicum.yandex.ru/android-developer/"
+            )
+            startActivity(Intent.createChooser(intentShare, "Поделиться через"))
+        }
+    }
+
+    fun setupContackSupport() {
+        val contactSupport = findViewById<TextView>(R.id.contactSupport)
+        contactSupport.setOnClickListener {
+            val intentSupport = Intent(Intent.ACTION_SENDTO).apply {
+                val studentEmail = "carrottochka@gmail.com"
+                val subject = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+                val body = "Спасибо разработчикам и разработчицам за крутое приложение!"
+                val uriText = "mailto:$studentEmail" +
+                        "?subject=${Uri.encode(subject)}" +
+                        "&body=${Uri.encode(body)}"
+                data = Uri.parse(uriText)
+                putExtra(
+                    Intent.EXTRA_SUBJECT, subject
+                )
+                putExtra(
+                    Intent.EXTRA_TEXT, body
+                )
+            }
+            try {
+                startActivity(Intent.createChooser(intentSupport, "Написать в поддержку"))
+            } catch (e: ActivityNotFoundException) {
+                Toast.makeText(
+                    this@SettingActivity,
+                    "Почтовое приложение не найдено",
+                    Toast.LENGTH_LONG
+                ).show()
+
+            }
 
         }
     }
+
+    fun setupUserAgreement() {
+        val userAgreement = findViewById<TextView>(R.id.userAgreement)
+        userAgreement.setOnClickListener {
+            val intentUserAgreement =
+                Intent(Intent.ACTION_VIEW, Uri.parse("https://yandex.ru/legal/practicum_offer/"))
+            startActivity(intentUserAgreement)
+        }
+    }
+
+
 }
